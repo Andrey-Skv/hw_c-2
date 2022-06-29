fda0c96a412e892c322dc1b6ea3e663373ef8745
Dron <epttit@gmail.com>
2022-06-29 20:34:58 +0300

    add task13

---
+++ task13/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+Console.WriteLine("введите число");
+int num =Convert.ToInt32(Console.ReadLine());
+
+void show3 (int num1)
+{
+  if (num>99)
+{
+    Console.WriteLine("Третья цифра в сумме "+num.ToString()+" = "+num.ToString()[2]);
+}
+    else
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+}
+show3(num);
