e56b4704255fef11121058d2199d8c78d75988a4
Dron <epttit@gmail.com>
2022-06-29 19:28:50 +0300

    add task15

---
+++ task15/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+void showWeekend()
+{
+
+int num = 0;
+Console.WriteLine("введите число от 1 до 7");
+num = Convert.ToInt32(Console.ReadLine());
+
+while (num < 1 | num > 7)
+{
+    Console.WriteLine("введите число от 1 до 7");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+if (num == 1)
+ {
+    Console.WriteLine("сегодня будний день");
+ }
+if (num == 2)
+  {
+    Console.WriteLine("сегодня будний день");
+  }
+  if (num == 3)
+  {
+    Console.WriteLine("сегодня будний день");
+  }
+  if (num == 4)
+  {
+    Console.WriteLine("сегодня будний день");
+  }
+  if (num == 5)
+  {
+    Console.WriteLine("сегодня будний день");
+  }
+  if (num == 6)
+  {
+    Console.WriteLine("сегодня выходной");
+  }
+  if (num == 7)
+  {
+    Console.WriteLine("сегодня выходной");
+  }
+}
+
+showWeekend();
